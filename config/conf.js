4a2411039aa28cb66555fbd1aabe076970d055f0
Abdessamad El Hafi <59848922+abdoelhafi@users.noreply.github.com>
2021-05-08 07:18:05 +0000

    Delete conf.js

--- config/conf.js
+++
@@ -1,7 +0,0 @@
-const keys = {
-    MongoURI: "mongodb+srv://abdo123:abdo456@cluster0.lgvs4.mongodb.net/Projects-db?retryWrites=true&w=majority",
-    // port: process.env.PORT ||5000,
-    port: 5000,
-    domainName : ["http://localhost",`${3000}/r`].join(':')
-}
-module.exports = keys;
